dd7b9c55d5cabb7f2e017c9f6bc58c25af908326
cfmessias <cfmessias@gmail.com>
2025-03-04 21:46:55 +0000

    Ajusta layout para slider na sidebar e gráficos lado a lado

--- 📗_top100.py
+++ 📗_top100.py
@@ -3,8 +3,6 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
 # Caminho absoluto para o diretório atual
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,10 +19,13 @@ st.title("Top 100 Trending Books Analysis")
 
 # Slider para faixa de preço na sidebar
 max_price = st.sidebar.slider("Price Range ($)", min_value=int(df_top100_books["book price"].min()), max_value=int(df_top100_books["book price"].max()), value=int(df_top100_books["book price"].max()))
-max_year=st.sidebar.slider("Year Range",min_value=int(df_top100_books["year of publication"].min()),max_value=int(df_top100_books["year of publication"].max()),value=int(df_top100_books["year of publication"].max()))
-# Filtrar os dados com base no valor do slider
-df_books = df_top100_books[df_top100_books["book price"] <= max_price]
+
+# Sliders para intervalo de anos de publicação na sidebar
+min_year, max_year = st.sidebar.slider("Year of Publication Range", min_value=int(df_top100_books["year of publication"].min()), max_value=int(df_top100_books["year of publication"].max()), value=(int(df_top100_books["year of publication"].min()), int(df_top100_books["year of publication"].max())))
+
+# Filtrar os dados com base nos valores dos sliders
 df_books = df_top100_books[(df_top100_books["book price"] <= max_price) & (df_top100_books["year of publication"] >= min_year) & (df_top100_books["year of publication"] <= max_year)]
+
 # Mostrar tabela filtrada
 st.write(df_books.head(10))
 
@@ -34,10 +35,9 @@ fig_prices = px.histogram(df_books["book price"], title="Book Price Distribution
 fig_ratings = px.bar(df_books["rating"].value_counts().sort_index(), title="Ratings Distribution")
 
 # Mostrar gráficos lado a lado
-col1, col2,col3 = st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
     st.plotly_chart(fig_years)
-with col2:
     st.plotly_chart(fig_prices)
-with col3:
+with col2:
     st.plotly_chart(fig_ratings)
